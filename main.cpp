e4e62645fcee59cdabb45ceb3d5d861f1fc7672d
Luiz Fernando <you@example.com>
2019-09-10 21:06:19 +0000

    vantagens sao vatagens meu caro

--- main.cpp
+++ main.cpp
@@ -490,7 +490,63 @@ void batalha(Carta carta1, Carta carta2){
 }
 
 Carta ataque(Carta carta1,Carta carta2){
-    carta2.hp = carta2.hp - carta1.ataque;
+    int atCarta1 = carta1.ataque;
+    string grass   = "GRASS     ";
+    string fire    = "FIRE      ";
+    string water   = "WATER     ";
+    string poison  = "POISON    ";
+    string bug     = "BUG       ";
+    string normal  = "NORMAL    ";
+    string psychic = "PSYCHIC   ";
+    string eletric = "ELECTRIC  ";
+    string ground  = "GROUND    ";
+    string fly     = "FLY       ";
+    string fairy   = "FAIRY     ";
+
+    if(carta1.tipo == grass){
+        if (carta2.tipo == water || carta2.tipo == eletric || carta2.tipo == ground || carta2.tipo == grass){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == fire){
+        if (carta2.tipo == fire || carta2.tipo == bug || carta2.tipo == grass){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == water){
+        if (carta2.tipo == fire || carta2.tipo == ground || carta2.tipo == water){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == poison){
+        if (carta2.tipo == fairy || carta2.tipo == grass || carta2.tipo == bug){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == bug){
+        if (carta2.tipo == psychic || carta2.tipo == grass || carta2.tipo == ground){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == psychic){
+        if (carta2.tipo == poison){
+            atCarta1 = atCarta1 + 10; 
+        }       
+    }else if(carta1.tipo == eletric){
+        if (carta2.tipo == fly || carta2.tipo == water){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == ground){
+        if (carta2.tipo == fire || carta2.tipo == normal || carta2.tipo == fly || carta2.tipo == poison || carta2.tipo == bug ){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == fly{
+        if (carta2.tipo == bug || carta2.tipo == grass){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else if(carta1.tipo == fairy){
+        if (carta2.tipo == bug){
+            atCarta1 = atCarta1 + 10; 
+        }
+    }else{
+    
+    }      
+    carta2.hp = carta2.hp - atCarta1;
     if(carta2.hp<=0){
         carta2.hp=0;
     }
